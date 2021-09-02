9dfb204e8b17cdf81cadab6302547d5d13459112
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2021-09-02 17:03:51 +0300

    Update realization of methods
    TODO: add more tests

--- Lesson2/LinkedList2.java
+++ Lesson2/LinkedList2.java
@@ -13,6 +13,7 @@ public LinkedList2()
   tail = null;
 }
 
+// Добавить в конец списка
 public void addInTail(Node a_item)
 {
   if (head == null) {
@@ -26,6 +27,7 @@ public void addInTail(Node a_item)
   this.tail = a_item;
 }
 
+// Поиск элемента по ключу
 public Node find(int a_value)
 {
   Node node = this.head;
@@ -41,10 +43,11 @@ public Node find(int a_value)
   return null;
 }
 
+// Поиск всех элементов в списке
 public ArrayList<Node> findAll(int a_value)
 {
   ArrayList<Node> nodesResult = new ArrayList<Node>();
-  Node node = this.head;
+  Node node = this.head ;
   for( int i = 0; node != null; node = node.next ){
     if ( node.value == a_value ){
       nodesResult.add(i, node);
@@ -54,46 +57,126 @@ public ArrayList<Node> findAll(int a_value)
   return nodesResult;
 }
 
-public boolean remove(int _value)
+// Удалить элемент из списка по ключу
+public boolean remove(int a_value)
 {
   if(this.count() == 0){
     return false;
   }
   
   Node node = this.head;
+  // Проверка головы списка
+  if(node.value == a_value){
+    this.head = node.next;
+    node.next = null;
+    node = this.head;
+    node.prev = null;
+    return true;
+  }
+  
+  // Проверка списка
   while (node != null) {
-
-
-
+    if(node.value == a_value){
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+      return true;
+    }
+    node = node.next;
   }
 
+  return false; // если узел небыл удалён
+}
 
-  return false; // если узел был удалён
-}
-public void removeAll(int _value)
+// Удаление всех элементов из списка по ключу
+public void removeAll(int a_value)
 {
-  // здесь будет ваш код удаления всех узлов по заданному значению
+  if(this.count() == 0){
+    
+    Node node = this.head;
+    // Проверка головы списка
+    while(node.value == a_value && node != null){
+      this.head = node.next;
+      node.next = null;
+      node = this.head;
+      node.prev = null;
+    }
+    
+    // Проверка списка
+    while (node != null) {
+      if(node.value == a_value){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        if(node.next == null){
+          this.tail = node.prev;
+        }
+        node.next = null;
+        node.prev = null;
+      }
+      node = node.next;
+    }
+  }
 }
+
+// Очистка списка
 public void clear()
 {
-  // здесь будет ваш код очистки всего списка
+  Node node = this.head;
+  while(node != null){
+    Node tmp = node.next;
+    node.prev = null;
+    node.next = null;
+    node = tmp;
+  }
+  this.head = null;
+  this.tail = null;
 }
 
+// Количество элементов в списке
 public int count()
 {
   int count = 0;
   Node node = this.head;
   while (node != null){
     count++;
+    node = node.next;
   }
   return count; 
 }
 
-public void insertAfter(Node _nodeAfter, Node _nodeToInsert)
+public void insertAfter(Node a_nodeAfter, Node a_nodeToInsert)
 {
-  // здесь будет ваш код вставки узла после заданного узла
-  // если _nodeAfter = null
-  // добавьте новый элемент первым в списке 
+  Node node = this.head;
+  // Проверка головы списка
+  if(node == a_nodeToInsert){
+    this.head = node.next;
+    node.next = null;
+    node = this.head;
+    node.prev = null;
+  } else while (node != null) {
+    if(node == a_nodeToInsert){
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+    node = node.next;
+  }
+
+  node = this.head;
+  if(a_nodeAfter == null) {
+
+    this.head.prev = a_nodeToInsert;
+    a_nodeToInsert.next = this.head;
+    a_nodeToInsert.prev = null;
+    if(this.count() == 1) this.tail = a_nodeToInsert;
+
+  } else while( node != null){
+      if ( node == a_nodeAfter ){
+        Node tmp = node.next;
+        node.next = a_nodeToInsert;
+        a_nodeToInsert.next = tmp;
+        a_nodeToInsert.prev = node;
+        if( tmp == null ){ this.tail = a_nodeToInsert; }
+      }
+  }
 }
 }
 
--- Lesson2/Main.java
+++ Lesson2/Main.java
@@ -4,38 +4,101 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void t_printList( LinkedList2 mylist ){
-        Node node = mylist.head;
+    public static void t_printList( LinkedList2 a_mylist ){
+        Node node = a_mylist.head;
         for (int i = 0; node != null; i++) {
             System.out.println("Element " + i + " = " + node.value);
             node = node.next;
         }
-        t_printHead(mylist);
-        t_printTail(mylist);
+        t_printHead(a_mylist);
+        t_printTail(a_mylist);
         System.out.println();
-
     }
 
-    public static void t_printHead( LinkedList2 mylist ){
-        if( mylist.head == null)         
-            System.out.println("This head now is null");
+    public static void t_printHead( LinkedList2 a_mylist ){
+        if( a_mylist.head == null)         
+            System.out.println("This head now is " + a_mylist.head);
         else 
-            System.out.println("This head now is " + mylist.head.value);
+            System.out.println("This head now is " + a_mylist.head.value);
     }
 
-    public static void t_printTail( LinkedList2 mylist ){
-        if( mylist.tail == null)         
-            System.out.println("This tail now is null");
+    public static void t_printTail( LinkedList2 a_mylist ){
+        if( a_mylist.tail == null)         
+            System.out.println("This tail now is " + a_mylist.tail);
         else 
-            System.out.println("This tail now is " + mylist.tail.value);
+            System.out.println("This tail now is " + a_mylist.tail.value);
     }
 
 /***********************************************************/
-
-  
-    public static void main(String[] args) {
+    public static void t_findAll( LinkedList2 a_mylist, int a_value ){
+        ArrayList<Node> resultList = new ArrayList<Node>();
+        resultList = a_mylist.findAll(a_value);
         
+        System.out.println(resultList);
 
+        for (Node x : resultList){
+            System.out.println( x + " : " + x.value + " : " + x.prev + " : " + x.next);
+        }
+        t_printHead(a_mylist);
+        t_printTail(a_mylist);
+    }
+  
+    public static void t_remove( LinkedList2 a_mylist, int a_value ){
+        System.out.println();
+        System.out.println("Удаление элемента по ключу = " + a_value);
+        a_mylist.remove(a_value);
+        System.out.println("Оставшиеся элементы: ");
+        t_printList(a_mylist);
+
+        System.out.println("Элемент перед head: " + a_mylist.head.prev);
+        System.out.println("Элемент после  tail: " + a_mylist.tail.next);
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList2 mylist = new LinkedList2();
+
+        mylist.addInTail( new Node(3) );
+        mylist.addInTail( new Node(2) );
+        mylist.addInTail( new Node(3) );
+        mylist.addInTail( new Node(4) );
+        mylist.addInTail( new Node(5) );
+        mylist.addInTail( new Node(6) );
+        mylist.addInTail( new Node(3) );
+        mylist.addInTail( new Node(8) );
+        mylist.addInTail( new Node(9) );
+        mylist.addInTail( new Node(3) );
+
+        t_printList(mylist);
+        System.out.println(mylist.find(3));
+        
+// Тест посика всех элементов по ключу
+        System.out.println();
+        // t_findAll(mylist, 3);
+
+// Тест удаления эелемента по ключу
+        //Удаление первого элемента в списке
+        t_remove(mylist, 3);
+
+        //Удаление элемента из пустого списка
+            // TODO
+
+        //Удаление элеменета из конца списка
+            // TODO
+
+        //Удаление последнего элемента списка
+                    // TODO
+
+// Тест удаления всех элементов по ключу
+        // TODO
+
+
+// Тест вставка элемента после элемента
+        // TODO
+
+
+        System.out.println();
     }
 
 
