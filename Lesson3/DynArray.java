aa424be351c2e724b1b1c691bc5a51fe010f5ac6
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2021-09-07 19:21:08 +0300

    Update class methods

--- Lesson3/DynArray.java
+++ Lesson3/DynArray.java
@@ -4,43 +4,112 @@ import java.lang.reflect.Array;
 
 public class DynArray<T>
 {
-  public T [] array;
-  public int count;
-  public int capacity;
-  Class clazz;
+  // Для тестов вернуть public а то хз
+  private T [] m_array;
+  private int m_count;
+  private int m_capacity;
+  private int m_minCapacity;
+  Class m_clazz;
 
-  public DynArray(Class clz)
+  /**
+   * Конструктор
+   * new DynArray<Integer>(Integer.class);
+   * @param a_clz
+   */
+  public DynArray(Class a_clz)
   {
-    clazz = clz; // нужен для безопасного приведения типов
-    // new DynArray<Integer>(Integer.class);
-    count = 0;
-    makeArray(16);
+    m_clazz = a_clz; // нужен для безопасного приведения типов
+    m_count = 0;
+    makeArray(m_minCapacity);
   }
 
-  public void makeArray(int new_capacity)
+  /****************************************************************
+   * Метод меняет размер массива array, копируя при необходимости текущие 
+   * объекты вышеописанным способом.
+   * увеличение буфера происходит в два раза, а уменьшение в полтора 
+   * раза (текущее значение размера буфера делится на 1.5, и результат 
+   * приводится к целому типу)
+   */
+  public void makeArray(int a_newCapacity)
   {
-    // array = (T[]) Array.newInstance(this.clazz, new_capacity);
-    // ваш код
+    if (m_count == 0){
+      m_array = ( T[] ) Array.newInstance( this.m_clazz, a_newCapacity );
+    } else {
+      T [] newArray = ( T[] ) Array.newInstance( this.m_clazz, a_newCapacity );
+      this.copy(newArray);
+      m_array = newArray;
+    } 
+      // ваш код
   }
 
-  public T getItem(int index)
+  /************************************************************
+   * Получение объекта по его индексу
+   * Встроить проверку корректности индекса в рамках границ, и 
+   * генерацию соответствующего исключения, если обращение некорректно;
+   */
+  public T getItem(int a_index)
   {
-    // ваш код
-    return null;
+    assert( a_index <= m_count && a_index >= 0);
+    return m_array[a_index];
   }
-
+  /*****************************************************************
+   * добавление нового элемента в конец массива
+   * 
+   * @param itm
+   */
   public void append(T itm)
   {
-    // ваш код
+    
+    if ( (m_count + 1) == m_capacity ) {
+      m_capacity *= 2;
+      makeArray(m_capacity);
+    } 
+    m_array[m_count] = itm;
+    m_count++;
   }
 
-  public void insert(T itm, int index)
+  /*****************************************************************
+   * Вставляет в i-ю позицию объект item, сдвигая вперёд все последующие 
+   * элементы. ! Новая длина массива может превысить размер буфера.
+   * 
+   * @param a_itm
+   * @param a_index
+   */
+  public void insert(T a_itm, int a_index)
   {
     // ваш код
   }
   
-  public void remove(int index)
+  /**
+   * удаляет объект из i-й позиции, при необходимости выполняя сжатие буфера.
+   * @param a_index
+   */
+  public void remove(int a_index)
   {
     // ваш код
+  }
+
+  /**
+   * Количество элементов в массиве
+   * @return
+   */
+  public int count(){
+    return m_count;
+  }
+
+  /**
+   * Объем буфера массива 
+   */
+  public int capacity(){
+    return m_capacity;
+  }
+
+  /**
+   * Копирование элементов масива
+   */
+  public void copy(T[] a_array){
+    for(int i = 0; i != count(); i++){
+      a_array[i] = m_array[i];
+    }
   }
 }
--- Lesson3/Main.java
+++ Lesson3/Main.java
@@ -4,4 +4,19 @@ package Algorithms.DynArray;
 
 public class Main {
     
+
+    
+    public static void main(String[] args) {
+        // Создание массива
+        DynArray<Integer> myArray =  new DynArray<Integer>(Integer.class);
+        myArray.append(1);
+        myArray.append(2);
+        myArray.append(3);
+        myArray.append(4);
+            
+        for(int i = 0; i != myArray.count(); i++){
+            System.out.println(myArray.getItem(i));
+        }
+        
+    }
 }
