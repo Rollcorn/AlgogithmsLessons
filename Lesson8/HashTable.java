6459ed625dd016ff22e934cf33676c40b2797f65
Rollcorn <lazydaimon@gmail.com>
2021-09-22 17:23:53 +0300

    Hash table 1st commit

--- Lesson8/HashTable.java
+++ Lesson8/HashTable.java
@@ -1,43 +1,83 @@
 package AlgorithmsLessons.Lesson8;
 
 public class HashTable
+{
+  public int size;
+  public int step;
+  public String [] slots; 
+
+  public HashTable(int sz, int stp)
   {
-    public int size;
-    public int step;
-    public String [] slots; 
-
-    public HashTable(int sz, int stp)
-    {
-      size = sz;
-      step = stp;
-      slots = new String[size];
-      for(int i=0; i < size; i++) slots[i] = null;
-    }
-
-    public int hashFun(String value)
-    {    
-         // всегда возвращает корректный индекс слота
-         return 0;
-    }
-
-    public int seekSlot(String value)
-    {
-         // находит индекс пустого слота для значения, или -1
-         return -1;
-    }
-
-     public int put(String value)
-     {
-         // записываем значение по хэш-функции
-         
-         // возвращается индекс слота или -1
-         // если из-за коллизий элемент не удаётся разместить 
-         return -1;
-     }
-
-     public int find(String value)
-     {
-         // находит индекс слота со значением, или -1
-         return -1;
-     }
+    size = sz;
+    step = stp;
+    slots = new String[size];
+    for(int i=0; i < size; i++) slots[i] = null;
   }
+
+  public int hashFun(String value)
+  {    
+      int res = 0;
+      for(int i = 0; i != value.length(); i++){
+          res += value.codePointAt(i);
+      }
+      res = res%size;
+      return res;
+  }
+
+  public int seekSlot(String a_value)
+  {
+    int slotNum = hashFun(a_value);
+    for(int count = 0; count <= size; count++){
+      
+      if(slots[slotNum] == null){
+        return slotNum;
+      }
+
+      if(slotNum + step > size - 1){
+        slotNum = slotNum + step - size;
+      } else {
+        slotNum += step;
+      }
+    }
+    return -1;
+  }
+  
+  public int put(String a_value)
+  {
+    int slotNum = seekSlot(a_value);
+    if( slotNum == -1){
+      return -1;
+    } else {
+      slots[slotNum] = a_value;
+      return slotNum;
+    }
+  }
+  
+  public int find(String a_value)
+  {
+    int slotNum = hashFun(a_value);
+
+    for(int count = 0; count <= size; count++){
+      
+      if(slots[slotNum] == a_value){
+        return slotNum;
+      }
+
+      if(slotNum + step > size - 1){
+        slotNum = slotNum + step - size;
+      } else {
+        slotNum += step;
+      }
+    }
+
+    return -1;
+  }
+
+  public void printTable(){
+    
+    for(int i = 0; i != size; i++){
+      System.out.println("The slot = " + i + " : " + slots[i]);
+    }
+    
+  }
+}
--- Lesson8/Main.java
+++ Lesson8/Main.java
@@ -2,7 +2,44 @@ package AlgorithmsLessons.Lesson8;
 
 public class Main {
     
+    public static void t_find(HashTable ht, String val){
+        System.out.println("The find slot = " + val + " : " + ht.find(val));
+
+    }
+
     public static void main(String[] args) {
+
+
         
+        HashTable mHTable = new HashTable(17, 3);
+
+        mHTable.put("Hello");
+        mHTable.put("my");
+        mHTable.put("name");
+        mHTable.put("is");
+        mHTable.put("Sarah");
+        mHTable.put("i");
+        mHTable.put("am");
+        mHTable.put("from");
+        mHTable.put("California");
+        mHTable.put("And");
+        mHTable.put("i");
+        mHTable.put("am");
+        mHTable.put("very");
+        mHTable.put("pleased");
+        mHTable.put("to");
+        mHTable.put("meet");
+        mHTable.put("you");
+
+        System.out.println("The hashcode = " + mHTable.hashFun("H"));
+
+        mHTable.printTable();
+
+        t_find(mHTable, "meet" );
+
+
+
+
+
     }
 }
